d1aa27553bef564522a56ee062fdc8cfbb227e49
pylypkutovoi <pylyp.kutov@gmail.com>
2018-11-15 21:23:56 +0100

    gulp test

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,23 @@
-var gulp = require('gulp');
-gulp.task('hello', function() {
-  console.log('Hello Zell');
+  var gulp = require("gulp");
+  var sass = require("gulp-sass");
+  var server = require("browser-sync").create();
+
+  gulp.task("sass", function(){
+    return gulp.src("source/sass/style.scss")
+      .pipe(sass())
+      .pipe(gulp.dest("source/css"));
+  });
+
+  gulp.task("server", function () {
+  server.init({
+    server: "source/",
+    notify: false,
+    open: true,
+    cors: true,
+    ui: false
+  });
+
+  gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("sass"));
+  gulp.watch("source/*.html").on("change", server.reload);
 });
+
